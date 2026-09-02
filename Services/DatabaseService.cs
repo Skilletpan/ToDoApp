2d61de733129b4a079e34359eb473763dcdcc3f0
Vivian Roth <vivian.roth@espas.ch>
2026-09-02 08:05:22 +0200

    Docstring improvements

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -14,13 +14,14 @@ public class DatabaseService(IConfiguration config) : IDatabaseService
     private readonly string ConnectionString = config.GetConnectionString("DB_CONN") ?? throw new ConfigurationErrorsException("Database Connection String (\"DB_CONN\") is missing!");
 
     /// <summary>
-    /// Runs an ExecuteAsync command on the database.
+    /// Runs an INSERT, UPDATE or DELETE command on the database.
     /// </summary>
     /// <param name="sql">The SQL command to run.</param>
     /// <param name="dto">An object holding the parameters to inject into the SQL command.</param>
     /// <returns>The amount of rows affected by the command.</returns>
     public async Task<int> Execute(string sql, object? dto = null)
     {
+        // Open connection and execute command
         await using var connection = new MySqlConnection(ConnectionString);
         {
             return await connection.ExecuteAsync(sql, dto);
@@ -28,14 +29,15 @@ public class DatabaseService(IConfiguration config) : IDatabaseService
     }
 
     /// <summary>
-    /// Runs a QueryFirstOrDefaultAsync command on the database.
+    /// Runs a SELECT command on the database.
     /// </summary>
     /// <typeparam name="T">The type of the queried object.</typeparam>
     /// <param name="sql">The SQL command to run.</param>
     /// <param name="dto">An object holding the parameters to inject into the SQL command.</param>
-    /// <returns>The queried object or null.</returns>
+    /// <returns>The first found queried object or null.</returns>
     public async Task<T?> QuerySingle<T>(string sql, object? dto = null)
     {
+        // Open connection and execute command
         await using var connection = new MySqlConnection(ConnectionString);
         {
             return await connection.QueryFirstOrDefaultAsync<T>(sql, dto);
@@ -43,7 +45,7 @@ public class DatabaseService(IConfiguration config) : IDatabaseService
     }
 
     /// <summary>
-    /// Runs a QueryAsync command on the database.
+    /// Runs a SELECT command on the database.
     /// </summary>
     /// <typeparam name="T">The type of the queried objects.</typeparam>
     /// <param name="sql">The SQL command to run.</param>
@@ -51,6 +53,7 @@ public class DatabaseService(IConfiguration config) : IDatabaseService
     /// <returns>A list containing the queried objects.</returns>
     public async Task<List<T>> QueryList<T>(string sql, object? dto = null)
     {
+        // Open connection and execute command
         await using var connection = new MySqlConnection(ConnectionString);
         {
             return (await connection.QueryAsync<T>(sql, dto)).ToList();
--- Services/Interfaces/IDatabaseService.cs
+++ Services/Interfaces/IDatabaseService.cs
@@ -3,7 +3,7 @@ namespace ToDoApp.Services.Interfaces;
 public interface IDatabaseService
 {
     /// <summary>
-    /// Runs an ExecuteAsync command on the database.
+    /// Runs an INSERT, UPDATE or DELETE command on the database.
     /// </summary>
     /// <param name="sql">The SQL command to run.</param>
     /// <param name="dto">An object holding the parameters to inject into the SQL command.</param>
@@ -11,16 +11,16 @@ public interface IDatabaseService
     public Task<int> Execute(string sql, object? dto = null);
 
     /// <summary>
-    /// Runs a QueryFirstOrDefaultAsync command on the database.
+    /// Runs a SELECT command on the database.
     /// </summary>
     /// <typeparam name="T">The type of the queried object.</typeparam>
     /// <param name="sql">The SQL command to run.</param>
     /// <param name="dto">An object holding the parameters to inject into the SQL command.</param>
-    /// <returns>The queried object or null.</returns>
+    /// <returns>The first found queried object or null.</returns>
     public Task<T?> QuerySingle<T>(string sql, object? dto = null);
 
     /// <summary>
-    /// Runs a QueryAsync command on the database.
+    /// Runs a SELECT command on the database.
     /// </summary>
     /// <typeparam name="T">The type of the queried objects.</typeparam>
     /// <param name="sql">The SQL command to run.</param>
