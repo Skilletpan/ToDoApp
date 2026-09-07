b34493f57045c0297282e813447e101457f004f3
Vivian Roth <vivian.roth@espas.ch>
2026-09-07 15:58:55 +0200

    Added Todo Items table in Frontend

---
+++ Components/Pages/Home.razor.cs
@@ -0,0 +1,17 @@
+using ToDoApp.Models;
+using ToDoApp.Services.Interfaces;
+
+namespace ToDoApp.Components.Pages;
+
+public partial class Home(ITodoService todoService)
+{
+    /// <summary>
+    /// The list of Todo items in the database.
+    /// </summary>
+    List<TodoModel> todoItems = [];
+
+    /// <summary>
+    /// Fetches all Todo items from the database.
+    /// </summary>
+    protected override async Task OnInitializedAsync() => todoItems = await todoService.FetchAllTodos();
+}
