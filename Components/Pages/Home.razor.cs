642edad960ea7b795752475f3823f1608d1b2972
Vivian Roth <vivian.roth@espas.ch>
2026-09-15 14:01:44 +0200

    Filter by name

--- Components/Pages/Home.razor.cs
+++ Components/Pages/Home.razor.cs
@@ -8,19 +8,36 @@ namespace ToDoApp.Components.Pages;
 public partial class Home(ITodoService todoService)
 {
     /// <summary>
-    /// The list of Todo items in the database.
-    /// </summary>
-    List<TodoModel> todoItems = [];
-
-    /// <summary>
     /// The Todo DTO to use in the editor.
     /// </summary>
     private TodoDTO EditorDTO { get; set; } = new();
 
     /// <summary>
+    /// The list of Todo items in the database.
+    /// </summary>
+    private List<TodoModel> TodoItems = [];
+
+    /// <summary>
+    /// The string to filter Todo item names buy.
+    /// </summary>
+    private string SearchName = string.Empty;
+
+    /// <summary>
+    /// The list of Todo items after applying filters.
+    /// </summary>
+    private List<TodoModel> FilteredTodoItems => TodoItems.Where(todoItem =>
+    {
+        // Check against name search value
+        if (!todoItem.Name.Contains(SearchName, StringComparison.CurrentCultureIgnoreCase)) return false;
+
+        // Return remaining items
+        return true;
+    }).ToList();
+
+    /// <summary>
     /// Saves the Todo item in the editor and resets the form.
     /// </summary>
-    async Task SubmitForm()
+    private async Task SubmitForm()
     {
         // Save Todo item and reset form
         await SaveTodo(EditorDTO);
@@ -28,26 +45,21 @@ public partial class Home(ITodoService todoService)
     }
 
     /// <summary>
-    /// Resets the form to its empty state.
-    /// </summary>
-    void ResetForm() => EditorDTO = new();
-
-    /// <summary>
     /// Saves or updates a Todo item in the database.
     /// </summary>
     /// <param name="dto">The Todo DTO to save.</param>
-    async Task SaveTodo(TodoDTO dto)
+    private async Task SaveTodo(TodoDTO dto)
     {
         // Save Todo item and refresh list
         await todoService.SaveTodo(dto);
-        todoItems = await todoService.FetchTodos();
+        TodoItems = await todoService.FetchTodos();
     }
 
     /// <summary>
     /// Progresses a Todo item to the next status.
     /// </summary>
     /// <param name="todo">The Todo item to progress.</param>
-    async Task ProgressTodo(TodoModel todo)
+    private async Task ProgressTodo(TodoModel todo)
     {
         // Catch Todo items with "Done" status
         if (todo.Status == TodoStatus.Done) throw new Exception("Cannot progress completed Todo item!");
@@ -64,15 +76,20 @@ public partial class Home(ITodoService todoService)
     /// Deletes a Todo item from the database.
     /// </summary>
     /// <param name="todo">The Todo item to delete.</param>
-    async Task DeleteTodo(TodoModel todo)
+    private async Task DeleteTodo(TodoModel todo)
     {
         // Delete Todo item and refresh list
         await todoService.DeleteTodo(todo.ID);
-        todoItems = await todoService.FetchTodos();
+        TodoItems = await todoService.FetchTodos();
     }
+
+    /// <summary>
+    /// Resets the form to its empty state.
+    /// </summary>
+    private void ResetForm() => EditorDTO = new();
 
     /// <summary>
     /// Fetches all Todo items from the database.
     /// </summary>
-    protected override async Task OnInitializedAsync() => todoItems = await todoService.FetchTodos();
+    protected override async Task OnInitializedAsync() => TodoItems = await todoService.FetchTodos();
 }
